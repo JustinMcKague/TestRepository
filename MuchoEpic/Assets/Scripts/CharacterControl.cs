07623ae8dec234d89d8ad0174c472c9ed7b607c4
Justin Mckague <mckaguej@shernet.sheridancollege.ca>
2020-01-10 10:01:18 -0500

    Some Script stuff

---
+++ MuchoEpic/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    public string charName = "Justin";
+    public int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(charName);
+    }
+}
